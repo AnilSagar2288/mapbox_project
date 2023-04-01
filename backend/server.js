92676ca645276406bcb9f6a838f37062fc440844
Anil Sagar <119804168+AnilSagar2288@users.noreply.github.com>
2023-04-01 17:13:05 +0530

    Deployment Changes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routers/userRoutes");
 const locationRoutes = require("./routers/locationRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path =require("path")
 const PORT = process.env.PORT || 8000;
 connectDB();
 const app = express();
@@ -15,6 +16,14 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/locations", locationRoutes);
+
+app.use(express.static(path.join(__dirname,"../frontend/build")))
+
+app.get("*", function(req,res){
+    res.sendFile(path.join(__dirname,"../frontend/build/index.html"))
+})
+
+
 
 app.use(notFound);
 app.use(errorHandler);
